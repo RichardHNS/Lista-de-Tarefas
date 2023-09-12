c8fa18a295711b17fc2f104efbf69f0ee9eccb4f
Richard Henriquee <richard.estudo08@gmail.com>
2023-09-12 14:34:45 -0300

    inserindo button remover

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,15 @@
 import React from "react";
 import { useState } from "react";
-import { Button, Button2, Container, Corpo, H1, Input, Item, Ul } from "./style";
+import {
+  Button,
+  Button2,
+  Container,
+  Corpo,
+  H1,
+  Input,
+  Item,
+  Ul,
+} from "./style";
 
 function App() {
   //Estados
@@ -16,23 +25,29 @@ function App() {
   function buttonClick() {
     setTasks([...tasks, inputValue]);
   }
+  function removeTask(index) {
+    setTasks(tasks.filter((_, i) => i !== index));
+  }
 
   return (
     <Container className="backDiv">
       <Corpo>
         <H1>Lista de Tarefas</H1>
-      <Input
-        placeholder="Digite Sua Tarefa"
-        type="text"
-        onChange={inputChange}
-      />
-      <Button onClick={buttonClick}>Adicionar</Button>
+        <Input
+          placeholder="Digite Sua Tarefa"
+          type="text"
+          onChange={inputChange}
+        />
+        <Button onClick={buttonClick}>Adicionar</Button>
 
-      <Ul>
-        {tasks.map((item, index) => (
-          <Item key={index} > {item} <Button2>x</Button2></Item>
-        ))}
-      </Ul>
+        <Ul>
+          {tasks.map((item, index) => (
+            <Item key={index}>
+              {" "}
+              {item} <Button2 onClick={() => removeTask(index)}>x</Button2>
+            </Item>
+          ))}
+        </Ul>
       </Corpo>
     </Container>
   );
